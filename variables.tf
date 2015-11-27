8e97c9d64fc29660b28639469b0e80d3e2881c52
engine07 <psihodelik@gmail.com>
2015-11-27 11:00:57 +0200

    details

--- variables.tf
+++ variables.tf
@@ -9,4 +9,4 @@ variable sub_domain {default=""}
 variable hosted_zone_id { default="Z18CFOC2QHH7FA"}
 
 variable aws_region {default="us-east-1"}
-variable ssh_key {default = "/home/ubuntu/microservices-infrastructure/expaas/deployer"}
+variable ssh_key {default = "deployer"}
